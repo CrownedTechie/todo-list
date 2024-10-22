936782ff6745fd75f804eb1dd6ffbffca1e6fa89
Crownedtechie <nenchi00@gmail.com>
2024-10-22 14:55:28 +0100

    --style:: implement the UI styling for the body

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import "./App.css";
+import Container from "./components/container/Container";
 
 function App() {
 
   return (
     <>
-      <h1>SIMPLE TODO LIST</h1>
+      <Container>
+        <header> 
+          <h1>TO-DO</h1>
+          <p>SEARCH BAR BTN</p>
+          <p className="welcome-message">WHAT'S UP, CHINENYE!</p>
+        </header>
+
+        <section>CATEGORIES</section>
+
+        <section className="task-section">
+          <div className="task-actions">
+            <button>NEW TASK</button>
+            <button>FILTERS</button>
+          </div>
+
+          
+          <ul>
+            <li>TODO LIST 1</li>
+            <li>TODO LIST 1</li>
+          </ul>
+        </section>
+      </Container>
     </>
   )
 }
---
+++ src/components/container/Container.tsx
@@ -0,0 +1,17 @@
+import styles from "./style.module.css";
+import { type ReactNode } from "react";
+
+type ContainerProps = {
+    children: ReactNode
+};
+
+
+const Container = ({children} : ContainerProps) => {
+    return ( 
+        <div className={styles.container}>
+            {children}
+        </div>
+     );
+}
+ 
+export default Container;
